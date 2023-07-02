45b09bc5726171748b0ef5dfe253b25c9f8ce456
Maksim <98279735+Maxnightik@users.noreply.github.com>
2023-07-02 19:26:29 +0300

    Add files via upload

---
+++ js/tabs.js
@@ -0,0 +1,29 @@
+const tabs = (option) => {
+  const {
+    selectorTabsButtons,
+    activeClassButton,
+    selectorTabsElement,
+    activeClassTab,
+    callback = () => {},
+  } = option;
+
+  const tabsBtns = document.querySelectorAll(selectorTabsButtons);
+  const tabsElems = document.querySelectorAll(selectorTabsElement);
+
+  tabsBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      for (let i = 0; i < tabsBtns.length; i++) {
+        if (tabsBtns[i] == btn) {
+          tabsBtns[i].classList.add(activeClassButton);
+          tabsElems[i].classList.add(activeClassTab);
+          callback(i);
+        } else {
+          tabsBtns[i].classList.remove(activeClassButton);
+          tabsElems[i].classList.remove(activeClassTab);
+        }
+      }
+    });
+  });
+};
+
+export default tabs;
